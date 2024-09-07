0aa3bcaac255101ac8f98b9af0fc9856eb85f89b
Larissa Albuquerque <106053968+lalbuquerques@users.noreply.github.com>
2024-09-07 12:25:14 -0300

    Atualização do exercicio 5-larissa
    Ampliando as funcionalidades do perfil personagem

--- flask/app.py
+++ flask/app.py
@@ -15,12 +15,47 @@ def get_list_characters_page():
 
 @app.route("/profile/<id>")
 def get_profile(id):
-    url = "https://rickandmortyapi.com/api/character/" + id
+    url = f"https://rickandmortyapi.com/api/character/{id}"
     response = urllib.request.urlopen(url)
     data = response.read()
-    dict = json.loads(data)
+    character = json.loads(data)
 
-    return render_template("profile.html", profile=dict)
+    # Obtendo informações de origem e localização
+    origin_url = character.get("origin", {}).get("url")
+    location_url = character.get("location", {}).get("url")
+
+    origin = {}
+    location = {}
+
+    if origin_url:
+        origin_response = urllib.request.urlopen(origin_url)
+        origin_data = origin_response.read()
+        origin = json.loads(origin_data)
+
+    if location_url:
+        location_response = urllib.request.urlopen(location_url)
+        location_data = location_response.read()
+        location = json.loads(location_data)
+
+    # Obtendo episódios em que o personagem aparece
+    episodes_info = []
+    for episode_url in character.get("episode", []):
+        episode_response = urllib.request.urlopen(episode_url)
+        episode_data = episode_response.read()
+        episode = json.loads(episode_data)
+        episodes_info.append({
+            "id": episode["id"],
+            "name": episode["name"],
+            "episode": episode["episode"],
+            "air_date": episode["air_date"]
+        })
+
+    return render_template("profile.html", 
+                           profile=character, 
+                           origin=origin, 
+                           location=location, 
+                           episodes=episodes_info)
+
 
 
 @app.route("/lista")
