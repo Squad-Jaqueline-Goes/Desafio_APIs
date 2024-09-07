e66ae76b8c7a3bbe4aac98a11a6cf20dd2dfffed
anadevti <anacarolayne777@gmail.com>
2024-09-07 16:33:30 -0300

    Revert "Merge branch 'dev' into item1-alice"
    This reverts commit 2a600ccc997cb96f879c35eff265df16f20244ee, reversing
    changes made to e3300a18979d13c43691586adb2ba73e62bfaf24.

--- flask/app.py
+++ flask/app.py
@@ -1,23 +1,94 @@
-@app.route("/episode/<id>")
-def get_episode(id):
-    url = "https://rickandmortyapi.com/api/episode/" + id
+from flask import Flask, render_template
+import urllib.request, json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_list_characters_page():
+    url = "https://rickandmortyapi.com/api/character/"
     response = urllib.request.urlopen(url)
-    episode_data = response.read()
-    episode_dict = json.loads(episode_data)
+    data = response.read()
+    dict = json.loads(data)
 
-    character_urls = episode_dict.get("characters", [])
-    characters_info = []
-    for char_url in character_urls:
-        char_id = char_url.split('/')[-1]
-        char_url_full = "https://rickandmortyapi.com/api/character/" + char_id
-        char_response = urllib.request.urlopen(char_url_full)
-        char_data = char_response.read()
-        char_dict = json.loads(char_data)
-        characters_info.append({
-            "id": char_id,
-            "name": char_dict.get("name"),
-            "image": char_dict.get("image"),
-            "profile_url": f'/profile/{char_id}'
-        })
+    return render_template("characters.html", characters=dict["results"])
 
-    return render_template("episode.html", episode=episode_dict, characters=characters_info)
+
+@app.route("/profile/<id>")
+def get_profile(id):
+    url = "https://rickandmortyapi.com/api/character/" + id
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template("profile.html", profile=dict)
+
+
+@app.route("/lista")
+def get_list_characters():
+    url = "https://rickandmortyapi.com/api/character/"
+    response = urllib.request.urlopen(url)
+    characters = response.read()
+    dict = json.loads(characters)
+
+    characters = []
+
+    for character in dict["results"]:
+        character = {
+            "name": character["name"],
+            "status": character["status"],
+        }
+        characters.append(character)
+
+    return {"characters": characters}
+
+
+# rota para location
+@app.route("/locations")
+def get_locations():  # função para listar as localizações
+    # var que recebera o valor da api
+    url = "https://rickandmortyapi.com/api/location"
+
+    # var que vai acessar a classe "urllib.request" para acessar a url
+    response = urllib.request.urlopen(url)
+
+    # var que fará a leitura dos resultados
+    locations = response.read()
+
+    # var para formatar os resultados no formato json
+    dict = json.loads(locations)
+
+    # dicionario de localizações
+    locations = []
+
+    for location in dict["results"]:
+        location = {
+            "id": location["id"],
+            "name": location["name"],
+            "type": location["type"],
+            "dimension": location["dimension"],
+        }
+        locations.append(location)
+        
+        #retorno da renderização da pag html com a lista de localizações
+        return render_template("locations.html", locations=locations)
+        
+@app.route("/episodes")
+def get_list_episodes():
+    url = "https://rickandmortyapi.com/api/episode/"
+    response = urllib.request.urlopen(url)
+    episodes = response.read()
+    dict = json.loads(episodes)
+
+    episodes = []
+
+    for episode in dict["results"]:
+        episode = {
+            "id": episode["id"],
+            "name": episode["name"],
+            "air_date": episode["air_date"],
+            "episode": episode["episode"],
+        }
+        episodes.append(episode)
+
+    return render_template("episodes.html", episodes=episodes)
