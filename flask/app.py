17049ef4924e4db9ba5311a03350195c4b89892d
anadevti <anacarolayne777@gmail.com>
2024-09-07 19:15:39 -0300

    adicionando nova page de desenvolvedoras e resolvendo bugs da aplicação

--- flask/app.py
+++ flask/app.py
@@ -23,6 +23,10 @@ def get_profile(id):
 
     return render_template("profile.html", profile=dict)
 
+@app.route("/desenvolvedoras/")
+def profile_dev():
+    return render_template("desenvolvedoras.html")
+
 
 @app.route("/lista")
 def get_list_characters():
@@ -92,3 +96,31 @@ def get_list_episodes():
         episodes.append(episode)
 
     return render_template("episodes.html", episodes=episodes)
+
+@app.route("/episode/<id>")
+def get_episode(id):
+    url = "https://rickandmortyapi.com/api/episode/" + id
+    response = urllib.request.urlopen(url)
+    episode_data = response.read()
+    episode_dict = json.loads(episode_data)
+
+    character_urls = episode_dict.get("characters", [])
+    characters_info = []
+    for char_url in character_urls:
+        char_id = char_url.split('/')[-1]
+        char_url_full = "https://rickandmortyapi.com/api/character/" + char_id
+        char_response = urllib.request.urlopen(char_url_full)
+        char_data = char_response.read()
+        char_dict = json.loads(char_data)
+        characters_info.append({
+            "id": char_id,
+            "name": char_dict.get("name"),
+            "image": char_dict.get("image"),
+            "profile_url": f'/profile/{char_id}'
+        })
+
+    return render_template("episode.html", episode=episode_dict, characters=characters_info)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
